fc0b5c97cc6f42f2890d462f3c2dab2ea1c5a684
Mona Lisa <Ayshyama@gmail.com>
2023-04-18 14:54:50 +0200

    update

--- Python/2-Py-Intro.py
+++ Python/2-Py-Intro.py
@@ -15,7 +15,7 @@ upgrading, and managing Python packages and their dependencies.
 
 '''
 # Task 1
-
+print('-----TASK 1-----')
 print (3)
 print(9.)
 print(.9)
@@ -23,9 +23,59 @@ print(1.09)
 print(9999_999_99999999.4)
 print('spam', 'eggs')
 print(True, False)
-'''
 
 
 # Task 2
-
+print('-----TASK 2-----')
 print(type(False))
+print(type(123))
+print(type('hello'))
+print(type(1.1))
+
+
+# Task 3
+print('-----TASK 3-----')
+print(isinstance(1.2345, bool))
+print(isinstance(1.2345, float))
+# Is 123 an instance of int? True
+print('Is', 123, 'an instance of int?', isinstance(123, int))
+
+
+# Task 5
+print('-----TASK 3-----')
+'''
+
+'''
+## Variables
+spam = 'eggs'
+print(spam, type(spam))
+
+spam = 42
+print(spam, type(spam))
+
+count = 1
+print(count + 1)`
+# print(count + 'spam')
+print(count + 1.7)
+print(count + .3)
+print(type(count + 7.))
+'''
+
+score = 10
+print(score - 51)
+print(score % 1)
+print(score % 2)
+print(score % 3)
+print(score % 4)
+print(score % 5)
+print(score % 6)
+print(score % 7)
+print(score % 8)
+print(score % 9)
+print(score % 10)
+
+print('----EXPONENTIAL----')
+print(score ** 2)
+print(score ** 3)
+print(score ** 4)
+print(score ** 5)
