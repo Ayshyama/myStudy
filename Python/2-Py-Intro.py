662d975f1135deccfc812ee555c286b8413eda04
Mona Lisa <Ayshyama@gmail.com>
2023-04-18 09:49:34 +0200

    update

---
+++ Python/2-Py-Intro.py
@@ -0,0 +1,31 @@
+### Teacher Joel Peltonen
+### 18.04.2023
+### Intro
+'''
+1. How to install Python on Linux:
+ sudo apt-get install python
+2. Why does one install different Py versions?
+Additionally, some libraries and frameworks may only work with certain versions of Python, 
+so installing different versions can help ensure compatibility with those tools.
+3. What is pip?
+Pip is a package manager for Python that simplifies the process of installing, 
+upgrading, and managing Python packages and their dependencies.
+3. 
+'''
+
+'''
+# Task 1
+
+print (3)
+print(9.)
+print(.9)
+print(1.09)
+print(9999_999_99999999.4)
+print('spam', 'eggs')
+print(True, False)
+'''
+
+
+# Task 2
+
+print(type(False))
