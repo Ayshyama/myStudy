29bbfb974ed5884b3efc53dd063a67a3f099160e
Igor <Ayshyama@gmail.com>
2023-06-06 21:25:51 +0200

    update

--- DCI/Python/23-Algorithmic-Thinking/exercise.py
+++ DCI/Python/23-Algorithmic-Thinking/exercise.py
@@ -1,16 +1,17 @@
 import matplotlib.pyplot as plt
-import math
+import mpl_toolkits.mplot3d
+import numpy as np
 
 X = [1, 2, 3, 4, 5]
 
 # Calculate Euler's number
-euler = math.exp(1)
+euler = np.exp(1)
 print(euler)
 
 # Calculate the y-values for each function
-Y_logN = [math.log(num) for num in X]
+Y_logN = [np.log(num) for num in X]
 Y_N = X
-Y_NlogN = [num * math.log(num) for num in X]
+Y_NlogN = [num * np.log(num) for num in X]
 Y_N_square = [num**2 for num in X]
 Y_N_exp = [euler**num for num in X]
 
@@ -25,25 +26,30 @@ def factorial(n):
 
 Y_N_fact = [factorial(num) for num in X]
 
-# Set up the plot
-plt.plot(X, Y_logN, label='logN')
-plt.plot(X, Y_N, label='N')
-plt.plot(X, Y_NlogN, label='NlogN')
-plt.plot(X, Y_N_square, label='N_square')
-plt.plot(X, Y_N_exp, label='N_exp')
-plt.plot(X, Y_N_fact, label='N!')
+# Set up the figure and axes for 3D plot
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
 
-# Set the x and y-axis labels
-plt.xlabel('X')
-plt.ylabel('Y')
+# Plot the functions in 3D
+ax.plot(X, Y_logN, np.zeros_like(X), label='logN')
+ax.plot(X, Y_N, np.zeros_like(X), label='N')
+ax.plot(X, Y_NlogN, np.zeros_like(X), label='NlogN')
+ax.plot(X, Y_N_square, np.zeros_like(X), label='N^2')
+ax.plot(X, Y_N_exp, np.zeros_like(X), label='N^e')
+ax.plot(X, Y_N_fact, np.zeros_like(X), label='N!')
+
+# Set the x, y, and z-axis labels
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
 
 # Set the axis limits
-plt.xlim(1, 4.0)
-plt.ylim(None, 50)  # y max - 50
+ax.set_xlim(1, 4.0)
+ax.set_ylim(-50, None)
+ax.set_zlim(0, None)
 
 # Add a legend
-plt.legend()
+ax.legend()
 
 # Show the plot
 plt.show()
-
