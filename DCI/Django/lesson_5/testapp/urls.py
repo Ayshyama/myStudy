b1f73fb8700246f6e8db01d453f9a89f15b0d0f3
Igor <Ayshyama@gmail.com>
2023-10-12 16:02:57 +0200

    update

--- DCI/Django/lesson_5/testapp/urls.py
+++ DCI/Django/lesson_5/testapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView, home_view, about_view
+from .views import HomePageView, AboutPageView, home_view, about_view, server_time_view
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('home2/', home_view, name='home2'),
     path('about2/', about_view, name='about2'),
     path('about/', AboutPageView.as_view(), name='about'),
+    path('server_time/', server_time_view, name='server_time'),
 ]
--- DCI/Django/lesson_5/testapp/views.py
+++ DCI/Django/lesson_5/testapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from datetime import datetime
+from django.http import HttpResponse
 
 # Create your views here.
 
@@ -8,17 +8,37 @@ from datetime import datetime
 class HomePageView(TemplateView):
     template_name = 'testapp/home.html'
 
+    def render_to_response(self, context, **response_kwargs):
+        print(self.request.COOKIES.get('my_cookie'))
+        if self.request.COOKIES.get('my_cookie') == 'my_cookie_value':
+            response = HttpResponse('WITH COOKIE!')
+            return response
+        return super().render_to_response(context, **response_kwargs)
+
 
 class AboutPageView(TemplateView):
     template_name = 'testapp/about.html'
 
 
 def home_view(request):
-    return render(request, 'testapp/home2.html', {"message": "Welcome to the homepage!"})
+    print(request)
+    print(request.method == "GET")
+    print(request.method)
+    print(request.COOKIES)
+    response = render(request, 'testapp/home2.html', {"message": "Welcome to the homepage!"})
+    print('Response:', response)
+    return response
+    # response_2 = HttpResponse("Welcome to the homepage!")
+    # response_2.write("<h1> Hello World! </h1>")
+    # return response_2
 
 
 def about_view(request):
     return render(request, 'testapp/about2.html', {"message": "About us page!"})
+
+
+def server_time_view(request):
+    return render(request, 'testapp/server_time.html')
 
 
 
